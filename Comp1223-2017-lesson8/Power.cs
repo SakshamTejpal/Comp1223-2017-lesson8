393cdc67c754fd8adf225546eebab0b9d2e9b35f
300932743 <300932743@PR-A315-27>
2017-07-11 17:40:41 -0400

    Power

---
+++ Comp1223-2017-lesson8/Power.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Saksham
+ * date
+ * Description
+ * version 0.1
+ **/
+
+namespace Comp1223_2017_lesson8
+{
+    /// <summary>
+    /// Power Class which will be used
+    /// </summary>
+    public class Power
+    {
+        //public properties
+        public string Name { get; set; }
+        public int rank { get; set; }
+        //constructor
+    }
+}
