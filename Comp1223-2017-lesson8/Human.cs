0c2a7f2ea40dee363779c9046096655b549ac7a4
300932743 <300932743@PR-A315-27>
2017-07-11 17:31:18 -0400

    modified

---
+++ Comp1223-2017-lesson8/Human.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Saksham
+ * date
+ * Description
+ * version 0.1
+ * */
+namespace Comp1223_2017_lesson8
+{
+    public abstract class Human
+    {
+        // private instance variable (Feilds)
+        private string _name;
+        //public properties
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+
+       
+        //constructor
+        public Human(string name)
+        {
+            this.Name = name;
+        }
+        
+    }
+}
