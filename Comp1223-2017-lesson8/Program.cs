c78e38b4aef35c3967b3678e7510f5d0371f9d1d
300932743 <300932743@PR-A315-27>
2017-07-11 17:10:48 -0400

    Add project files.

---
+++ Comp1223-2017-lesson8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp1223_2017_lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
