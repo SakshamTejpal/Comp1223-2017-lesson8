19fb7f0848bc6c1dbb92bf95c9d48b2a304fe6b3
300932743 <300932743@PR-A315-27>
2017-07-11 17:35:49 -0400

    SuperHuman

---
+++ Comp1223-2017-lesson8/Superhuman.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Saksham
+ * date
+ * Description
+ * version 0.1
+ * */
+namespace Comp1223_2017_lesson8
+{
+    public class Superhuman : Human
+    {
+        //private instance variables
+        //public properties
+        //constructors
+        public Superhuman(string name):base(name)
+        {
+
+        }
+        //private method
+        //public methods
+
+    }
+}
